3ed54d0d14148132c7f473dd9bc749c10490f3e3
Debora <pampanini.debora@gmail.com>
2023-09-01 09:48:28 -0300

    initial commit

---
+++ assets/assets/script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  /* jeito que explica o que esta sendo feito
+ 
+ if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }*/
+
+  /*automatico da programação*/
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  //substituir a img
+  if (html.classList.contains("light")) {
+    //se tiver light mode pegar a nova img
+    img.setAttribute("src", "./assets/assets/avatar-light.png")
+  } else {
+    //se não tiver light mode permanece a mesma img
+    img.setAttribute("src", "./assets/assets/avatar.png")
+  }
+}
