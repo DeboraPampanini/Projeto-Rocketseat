62ebba3140e7060cab8e3726e9ebbb288b92b7f2
Debora <pampanini.debora@gmail.com>
2023-09-01 14:30:39 -0300

    add readme

--- assets/assets/script.js
+++ assets/assets/script.js
@@ -13,6 +13,7 @@ function toggleMode() {
   html.classList.toggle("light")
 
   //pegar a tag img
+  
   const img = document.querySelector("#profile img")
   //substituir a img
   if (html.classList.contains("light")) {
